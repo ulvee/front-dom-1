f80e20b74ebde8b2ec81c3b4f3a971369f3ac14b
royalmammadov756r <126593256+royalmammadov756r@users.noreply.github.com>
2023-04-27 11:58:22 +0400

    signup

--- script.js
+++ script.js
@@ -1,78 +1,101 @@
 const darkMode = document.getElementById("darkMode");
 const lightMode = document.getElementById("lightMode");
 const body = document.getElementById("body");
+const inputElementName = document.getElementById("name-input");
+const inputElementPass = document.getElementById("pass-input");
+const inputElementEmail = document.getElementById("email-input");
+const nameError = document.querySelector(".name-error");
+const emailError = document.querySelector(".email-error");
+const passError = document.querySelector(".password-error");
 
-darkMode.onclick = () => {
+const form = document.querySelector("form");
+
+const emailRegex = /[A-Za-z0-9.]+[.]?[A-Za-z0-9]+@(code.edu.az)/;
+
+darkMode.addEventListener("click", () => {
   darkMode.classList.remove("active");
   lightMode.classList.add("active");
   body.classList.add("dark");
-};
-lightMode.onclick = () => {
+});
+
+lightMode.addEventListener("click", () => {
   lightMode.classList.remove("active");
   darkMode.classList.add("active");
   body.classList.remove("dark");
-  body.classList.add("light")
-};
-
-
-let inputElementName = document.getElementById("name-input");
-inputElementName.addEventListener("input", function () {
-  if (this.value.length > 10 || this.value.length == 0) {
-    this.style.border = "2px solid red";
-    this.style.backgroundColor = "white";
-    this.style.color = "black";
-  }
- else {
-    this.style.border = "";
-    this.style.backgroundColor = "";
-    this.style.color = "";
-  }
+  body.classList.add("light");
 });
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-let inputElementPass = document.getElementById("pass-input");
-inputElementPass.addEventListener("input", function () {
-  if (this.value.length < 5) {
-    this.style.border = "2px solid red";
-    this.style.backgroundColor = "white";
-    this.style.color = "black";
+  if (!inputElementName.value.trim()) {
+    nameError.innerText = "Name cannot be empty";
+    inputElementName.style.border = "2px solid red";
   } else {
-    this.style.border = "";
-    this.style.backgroundColor = "";
-    this.style.color = "";
+    nameError.innerText = "";
+    inputElementName.style.border = "";
   }
-});
 
-
-let inputElementEmail = document.getElementById("email-input");
-const emailRegex = /[A-Za-z0-9.]+[.]?[A-Za-z0-9]+@(code.edu.az)/;
-inputElementEmail.addEventListener("input", function () {
-  if (this.value.length == 0) {
-    this.style.border = "2px solid red";
-    this.style.backgroundColor = "white";
-    this.style.color = "black";
-  }
-  if (!emailRegex.test(this.value)) {
-    this.style.border = "2px solid red";
-    this.style.backgroundColor = "white";
-    this.style.color = "black";
+  if (inputElementEmail.value.trim() == "") {
+    emailError.innerText = "Email cannot be empty";
+  } else if (!validateEmail(inputElementEmail.value)) {
+    emailError.innerText = "Mail is not valid";
   } else {
-    this.style.border = "";
-    this.style.backgroundColor = "";
-    this.style.color = "";
+    emailError.innerText = "";
   }
-});
 
-
-let inputElementLogin = document.getElementById("login-input");
-inputElementLogin.addEventListener("input", function () {
-  if (this.value.length == 0) {
-    this.style.border = "2px solid red";
-    this.style.backgroundColor = "white";
-    this.style.color = "black";
+  if (!inputElementPass.value) {
+    passError.innerText = "Pass can not be empty";
+  } else if (inputElementPass.value.length < 5) {
+    passError.innerText = "password should be more than 5 char";
   } else {
-    this.style.border = "";
-    this.style.backgroundColor = "";
-    this.style.color = "";
+    passError.innerText = "";
   }
 });
+
+function validateEmail(email) {
+  return email.match(
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+}
+
+// inputElementPass.addEventListener("input", function () {
+//   if (this.value.length < 5) {
+//     this.style.border = "2px solid red";
+//     this.style.backgroundColor = "white";
+//     this.style.color = "black";
+//   } else {
+//     this.style.border = "";
+//     this.style.backgroundColor = "";
+//     this.style.color = "";
+//   }
+// });
+
+// inputElementEmail.addEventListener("input", function () {
+//   if (this.value.length == 0) {
+//     this.style.border = "2px solid red";
+//     this.style.backgroundColor = "white";
+//     this.style.color = "black";
+//   }
+//   if (!emailRegex.test(this.value)) {
+//     this.style.border = "2px solid red";
+//     this.style.backgroundColor = "white";
+//     this.style.color = "black";
+//   } else {
+//     this.style.border = "";
+//     this.style.backgroundColor = "";
+//     this.style.color = "";
+//   }
+// });
+
+// inputElementLogin.addEventListener("input", function () {
+//   if (this.value.length == 0) {
+//     this.style.border = "2px solid red";
+//     this.style.backgroundColor = "white";
+//     this.style.color = "black";
+//   } else {
+//     this.style.border = "";
+//     this.style.backgroundColor = "";
+//     this.style.color = "";
+//   }
+// });
